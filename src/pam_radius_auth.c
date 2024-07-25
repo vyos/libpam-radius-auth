15b5a25b3ce48cc9cf6aec7e9fbd736df833450a
Alan T. DeKok <aland@freeradius.org>
2024-07-25 08:39:14 -0700

    set Message-Authenticator to zero each time we send a packet.  Fixes #96

--- src/pam_radius_auth.c
+++ src/pam_radius_auth.c
@@ -1130,6 +1130,7 @@ static int talk_radius(radius_conf_t * conf, AUTH_HDR * request,
 		}
 
 		if (request->code == PW_AUTHENTICATION_REQUEST) {
+			memset(conf->message_authenticator, 0, AUTH_VECTOR_LEN);
 			hmac_md5(conf->message_authenticator, (uint8_t *) request, ntohs(request->length),
 				 (const uint8_t *) server->secret, strlen(server->secret));
 
